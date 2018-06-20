d601b7745538e97f6c8ea7089a96c9d11386748d
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-06-20 20:42:24 +0530

    Update and rename IntCount.java to IntCount1.java

---
+++ IntCount1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class IntCount1
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        String a1=Integer.toString(a);
+        int len=a1.length();
+        System.out.print(len);
+    }
+}
